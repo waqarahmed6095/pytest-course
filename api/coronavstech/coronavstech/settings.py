00adb565b1494070b53c4cebd96c3dbbf72d954d
Waqar Ahmed <waqarahmed695@gmail.com>
2025-05-27 23:12:46 +0300

    Add Bandit and other dependencies to project configuration
    - Added Bandit version 1.8.3 to the project dependencies for enhanced security analysis.
    - Updated the Django CI workflow to run Bandit checks while skipping specific rule B101.
    - Introduced additional dependencies including markdown-it-py, pbr, rich, and pyyaml with their respective versions to support project requirements.

--- api/coronavstech/coronavstech/settings.py
+++ api/coronavstech/coronavstech/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-^ni8_%5u4_gd=r@)s#9gkej_20r45-d!xyu3(f%ej8m^_79m00"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
