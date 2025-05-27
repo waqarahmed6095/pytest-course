f468ecd81a0bb241a3100088bac74054889b5d8f
Waqar Ahmed <waqarahmed695@gmail.com>
2025-05-27 17:00:12 +0300

    Refactor email unit tests to use pytest style assertions
    - Converted test methods in test_emails.py to use pytest-style assertions for improved readability and consistency.
    - Updated the test structure to remove the TestCase class and utilize fixtures for settings and client.

--- api/coronavstech/companies/tests/test_emails.py
+++ api/coronavstech/companies/tests/test_emails.py
@@ -1,46 +1,43 @@
-from django.test import TestCase
 from django.core import mail
-from django.test import Client
 import json
 from unittest.mock import patch
 
 
-class EmailUnittest(TestCase):
-    def test_send_email_should_succeed(self) -> None:
-        with self.settings(
-            EMAIL_BACKEND="django.core.mail.backends.locmem.EmailBackend"
-        ):
-            self.assertEqual(len(mail.outbox), 0)
-            mail.send_mail(
-                subject="Test Email",
-                message="This is a test email",
-                from_email="test@gmail.com",
-                recipient_list=["test@gmail.com"],
-            )
-            self.assertEqual(len(mail.outbox), 1)
-            self.assertEqual(mail.outbox[0].subject, "Test Email")
-            self.assertEqual(mail.outbox[0].body, "This is a test email")
-            self.assertEqual(mail.outbox[0].from_email, "test@gmail.com")
-            self.assertEqual(mail.outbox[0].to, ["test@gmail.com"])
 
-    def test_send_email_without_arguments_should_send_empty_email(self) -> None:
-        client = Client()
-        with patch("api.coronavstech.companies.views.send_mail") as mock_send_mail:
-            response = client.post(path="/send-email/", data={})
-            response_content = json.loads(response.content)
-            self.assertEqual(response.status_code, 200)
-            self.assertEqual(response_content["status"], "success")
-            self.assertEqual(response_content["info"], "email sent successfully")
-            mock_send_mail.assert_called_with(
-                subject=None,
-                message=None,
-                from_email="waqarahmed695@gmail.com",
-                recipient_list=["waqarahmed695@gmail.com"],
-            )
+def test_send_email_should_succeed(mailoutbox, settings) -> None:
 
-    def test_send_email_with_get_verb_should_fail(self) -> None:
-        client = Client()
-        response = client.get(path="/send-email/")
-        self.assertEqual(response.status_code, 405)
+    settings.EMAIL_BACKEND = "django.core.mail.backends.locmem.EmailBackend"
+    assert len(mailoutbox) == 0
+    mail.send_mail(
+        subject="Test Email",
+        message="This is a test email",
+        from_email="test@gmail.com",
+        recipient_list=["test@gmail.com"],
+    )
+    assert len(mailoutbox) == 1
+    assert mailoutbox[0].subject == "Test Email"
+    assert mailoutbox[0].body == "This is a test email"
+    assert mailoutbox[0].from_email == "test@gmail.com"
+    assert mailoutbox[0].to == ["test@gmail.com"]
+
+
+def test_send_email_without_arguments_should_send_empty_email(client) -> None:
+    with patch("api.coronavstech.companies.views.send_mail") as mock_send_mail:
+        response = client.post(path="/send-email/", data={})
         response_content = json.loads(response.content)
-        self.assertEqual(response_content["detail"], 'Method "GET" not allowed.')
+        assert response.status_code == 200
+        assert response_content["status"] == "success"
+        assert response_content["info"] == "email sent successfully"
+        mock_send_mail.assert_called_with(
+            subject=None,
+            message=None,
+            from_email="waqarahmed695@gmail.com",
+            recipient_list=["waqarahmed695@gmail.com"],
+        )
+
+
+def test_send_email_with_get_verb_should_fail(client) -> None:
+    response = client.get(path="/send-email/")
+    assert response.status_code == 405
+    response_content = json.loads(response.content)
+    assert response_content["detail"] == 'Method "GET" not allowed.'
