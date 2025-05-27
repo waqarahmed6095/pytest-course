bc5faa9f53eac1266fec9f6930973e774992316e
Waqar Ahmed <waqarahmed695@gmail.com>
2025-05-27 14:16:38 +0300

    Update ROOT_URLCONF in settings.py to match new project structure

--- api/coronavstech/coronavstech/settings.py
+++ api/coronavstech/coronavstech/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "coronavstech.urls"
+ROOT_URLCONF = "api.coronavstech.coronavstech.urls"
 
 TEMPLATES = [
     {
