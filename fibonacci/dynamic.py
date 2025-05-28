790a7b373e122a1be705b35be745c35c10f461cb
Waqar Ahmed <waqarahmed695@gmail.com>
2025-05-28 11:41:09 +0300

    Refactor Fibonacci dynamic functions for code consistency
    - Cleaned up spacing and formatting in the fibonacci_dynamic and fibonacci_dynamic_V2 functions for improved readability.
    - Ensured consistent use of whitespace in the code, enhancing maintainability.

--- fibonacci/dynamic.py
+++ fibonacci/dynamic.py
@@ -1,10 +1,10 @@
-
 def fibonacci_dynamic(n: int) -> int:
     fib_list = [0, 1]
-    
+
     for i in range(1, n + 1):
-        fib_list.append(fib_list[i ] + fib_list[i - 1])
+        fib_list.append(fib_list[i] + fib_list[i - 1])
     return fib_list[n]
+
 
 def fibonacci_dynamic_V2(n: int) -> int:
     fib_1 = 0
